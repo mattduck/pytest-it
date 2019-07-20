863963b23713feeb4eb46060639b4c0a811a8175
Matthew Duck <matt@mattduck.com>
2019-07-20 21:52:56 +0100

    Release: 0.1.1
    This just includes the README change - I'm pushing to fix the
    formatting on pypi.org.

--- src/pytest_it/__init__.py
+++ src/pytest_it/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __title__ = "pytest-it"
 __description__ = "Pytest plugin to display test reports as a plaintext spec, inspired by Rspec: https://github.com/mattduck/pytest-it."
